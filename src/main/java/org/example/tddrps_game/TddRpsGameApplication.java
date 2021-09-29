57ff20f635d30c5038b926788c672fe3f36d5251
Chanzhou Li <leon3309@gmail.com>
2021-09-29 02:55:51 +0200

    Initial commit

---
+++ src/main/java/org/example/tddrps_game/TddRpsGameApplication.java
@@ -0,0 +1,13 @@
+package org.example.tddrps_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddRpsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddRpsGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/tddrps_game/TddRpsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tddrps_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddRpsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
